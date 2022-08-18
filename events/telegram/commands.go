25feec10ea5b0b1d2577d656ce81e12c24e801f0
dimmWork <witty.sock3113@fastmail.com>
2022-08-18 19:11:54 +0200

    savePage() implementation

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"log"
+	"net/url"
 	"strings"
 )
 
@@ -17,6 +18,9 @@ func (p *Processor) doCmd(text string, chatID int, username string) error {
 	log.Printf("got new command '%s' from '%s'", text, username)
 
 	//	add page: http://
+	if isAddCmd(text) {
+		//	TODO: AddPage()
+	}
 
 	//	rnd page: /rnd
 	// help: /help
@@ -27,5 +31,21 @@ func (p *Processor) doCmd(text string, chatID int, username string) error {
 	case StartCmd:
 
 	}
+}
 
+//todo: lesson #5 15:30
+//	https://www.youtube.com/watch?v=f_esRaDae44&list=PLFAQFisfyqlWDwouVTUztKX2wUjYQ4T3l&index=5
+func (receiver Meta) savePage() {
+
+}
+
+func isAddCmd(text string) bool {
+	return isURL(text)
+}
+
+func isURL(text string) bool {
+	//	ya.ru don't valid for this type of cheking
+	//	http(s)://ya.ru valid because prefix
+	u, err := url.Parse(text)
+	return err == nil && u.Host != ""
 }
